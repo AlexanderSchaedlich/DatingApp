b3b9a023a4d66be14c869f390fee2fb6c3f078d6
Alexander Schaedlich <alexander.schaedlich@gmail.com>
2022-03-09 15:38:31 +0100

    End of section 8

---
+++ API/Data/Helpers/AutomapperProfiles.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Data.Helpers
+{
+    public class AutomapperProfiles : Profile
+    {
+        public AutomapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(
+                    destination => destination.PhotoUrl, 
+                    options => options.MapFrom(source => source.Photos != null 
+                        ? source.Photos.First(photo => photo.IsMain).Url
+                        : null
+                    )
+                )
+                .ForMember(
+                    destination => destination.Age,
+                    options => options.MapFrom(source => source.DateOfBirth.CalculateAge())
+                );
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
