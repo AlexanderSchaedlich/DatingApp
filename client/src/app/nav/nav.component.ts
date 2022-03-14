48150bdf132a0f50a06562e2cffa4db0a4450633
Alexander Schaedlich <alexander.schaedlich@gmail.com>
2022-03-14 11:49:41 +0100

    End of section 9

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(response => {
+    this.accountService.login(this.model).subscribe(() => {
       this.router.navigateByUrl('/members');
     });
   }
